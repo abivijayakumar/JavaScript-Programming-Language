da30fd53f9562c12a2cae5cd6040e12eba6c1f29
Abinaya <abivijayakumar419@gmail.com>
2025-08-10 19:30:04 +0530

    Create Functions3.java

---
+++ Functions3.java
@@ -0,0 +1,20 @@
+Function Samsung()
+  {
+  console.log("This is Samsung phone")
+    }
+Function apple()
+  {
+  console.log("This is apple")
+    }
+Function oppo()
+  {
+  console.log("This is oppo")
+    }
+Function redmi()
+  {
+  console.log("This is redmi")
+    }
+Samsung()
+  apple()
+  oppo()
+  redmi()
